65faae6721ccc0e001214a5676b10327c14c6f3d
Ken Bedwell <kenneth.bedwell@gmail.com>
2015-12-19 21:41:35 -0500

    changed to brew's go and had to change the path.

--- config/settings.el
+++ config/settings.el
@@ -1,6 +1,6 @@
 ;; go-pack settings
 
-(setq exec-path (cons "/usr/local/go/bin" exec-path))
+(setq exec-path (cons "/usr/local/opt/go/libexec/bin" exec-path))
 (add-to-list 'exec-path "/Users/kbedwell/dev/godev/bin")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
